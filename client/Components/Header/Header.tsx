8b15d50c5c4bf8cbd7ab53424aca7dfe1fff4949
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-12-15 16:24:04 -0600

    setup GET route and test DB to confirm functionality

--- client/Components/Header/Header.tsx
+++ client/Components/Header/Header.tsx
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Header: React.FC = () => {
     const [numCounter, setNumCounter] = useState<number>(0);
+    
+    // const handleClick = () => {
+    //     console.log('in handleclick');
+    //     const response:any = axios.get('/users');
+    // };
+
+    async function handleClick() {
+        console.log('in handle click');
+        try {
+            let users = await axios.get('/users');
+            console.log('users in handle click get: ', users.data);
+        }catch(error) {
+            console.log('error in handleclick')
+        }
+    }
+
 
     return (
         <div>
@@ -9,8 +26,11 @@ const Header: React.FC = () => {
             <h2>{numCounter}</h2>
             <button onClick={()=> {setNumCounter(numCounter + 1)}}>Count up</button>
             <button onClick={()=> {setNumCounter(numCounter - 1)}}>Count down</button>
+            <button onClick={handleClick}>Server Get</button>
         </div>
     )
 }
 
 export default Header;
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import path from "path";
+import pool from "./Modules/pool";
 
 const app = express();
+const router = express.Router();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -11,6 +13,17 @@ app.get("/", (req: Request, res: Response, next: NextFunction): void => {
     } catch (error) {
         next(error);
     }
+});
+
+app.get('/users', (req: express.Request, res: express.Response) =>{
+    console.log('in router.get');
+    pool.query('SELECT * FROM "user";').then((results:any) => {
+        console.log('results.data', results.rows);
+        res.send(results.rows);
+    }).catch((error:any) => {
+        console.log('error GETing, ', error);
+        res.sendStatus(500)
+    });
 });
 
 const PORT = 3000;
