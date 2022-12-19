50ea8ab8d47a24582c79aeeb1b2f9fcd4e92b99d
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-12-19 07:51:58 -0600

    router/routes setup in preperation for navbar links

--- client/Components/App/App.tsx
+++ client/Components/App/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import Header from '../Header/Header';
-import Playground from '../Playground/Playground';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+// components and pages
+import NavBar from '../NavBar/NavBar';
+import LandingPage from '../LandingPage/LandingPage';
+import RegisterPage from '../RegisterPage/RegisterPage';
+import LoginPage from '../LoginPage/LoginPage';
+
+
 
 const App: React.FC = () => {
     return (
-        <>
-            <Header />
-            <Playground />
-        </>
-    )
-}
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Navigate replace to="/home" />} />
+                <Route path='/home' element={<LandingPage />} />
+                <Route path='/register' element={<RegisterPage />} />
+                <Route path='/login' element={<LoginPage />} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
--- tasklist.md
+++ tasklist.md
@@ -1,9 +1,15 @@
-[] setup and test DB
+[x] setup and test DB
+[] setup navbar - simple
+    [] login/logout
+    [] My Leagues
+    [] About
+    [] style navbar for mobile
+[] create routes
 [] Login Form
     [] component
-    [] routes
+    [x] routes
     [] user authentication (review solo-first-look and Passport)
 [] Register user form
     [] component
-    [] POST route
+    [x] POST route
     [] auto-login when registering
