071d14b2dc2f61020ae37462dd3bee29bf74aba6
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-12-20 08:13:57 -0600

    css loader setup and tested

--- client/Components/LandingPage/LandingPage.tsx
+++ client/Components/LandingPage/LandingPage.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { teal } from '@mui/material/colors';
+
+const color = teal[50];
+
+
 
 const LandingPage = () => {
     return (
-        <div>LandingPage</div>
+        <div
+            className='landingPageDiv'
+            style={{
+                backgroundColor: color,
+                backgroundSize: "cover",
+                height: "70vh"
+            }}
+        >
+            LandingPage
+            <img src="/client/images/football_player_shadow.jpeg" alt="football Image" />
+        </div>
     )
 }
 
