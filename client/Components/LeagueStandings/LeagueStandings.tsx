2e86579538dd3498f89489d0b5a0a4316c4715f8
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-12-26 08:02:35 -0600

    logic for score calculation created and tested

--- client/Components/LeagueStandings/LeagueStandings.tsx
+++ client/Components/LeagueStandings/LeagueStandings.tsx
@@ -9,12 +9,36 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { useDispatch } from 'react-redux';
 
 const LeagueStandings = () => {
     const store: any = useSelector(store => store);
     const leagueUsers = store.leagues.currentLeagueUsers;
-    const dispatch = useDispatch();
+    const leagueDetail = store.leagues.leagueDetail;
+    const gameData = store.gameData.gameData;
+
+
+    const score = () => {
+        leagueUsers.map((user) => {
+            let score: number = 0;
+            let bonusCheck: { id: number, week: number, team: string, is_winner: boolean }[] = [];
+            const userPicks = leagueDetail.filter((pick: any) => pick.username === user.username);
+            userPicks.map((pick: any) => {
+                const pickStatus = gameData.filter((obj: any) => obj.week === pick.week && obj.team === pick.team);
+                if (pickStatus[0]?.is_winner) {
+                    bonusCheck.push(pickStatus[0]);
+                    score += pick.amount;
+                };
+            });
+            for (let i = 1; i <= 18; i++) {
+                if (bonusCheck?.filter((pick: any) => pick.week === i && pick.is_winner === true).length === 3) {
+                    console.log('found bonus score, user week:', user, i);
+                    score += 2;
+                };
+            };
+            console.log('score for user(with bonus)', user.username, score);
+        });
+    };
+
 
     return (
         <Box width={'80%'}>
@@ -28,7 +52,7 @@ const LeagueStandings = () => {
                     </TableHead>
                     <TableBody>
                         {leagueUsers.map((user) => {
-                            return <TableRow key={user.username}>
+                            return <TableRow key={user.username} onClick={score}>
                                 <TableCell>{user.username}</TableCell><TableCell align='right'>0</TableCell>
                             </TableRow>
                         })}
