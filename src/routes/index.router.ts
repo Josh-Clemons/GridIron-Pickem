6782dbc6f63a7df4b9403ce6dc5478c1db3d46ed
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2023-01-17 18:42:02 -0600

    Heroku

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { userRouter } from './user.router';
-import { leagueRouter } from './league.router';
+import { leagueRouter } from '../../dist/Routes/league.router';
 import { pickRouter } from './pick.router';
-import { dataRouter } from './api.data.router';
+import { dataRouter } from '../../dist/Routes/api.data.router';
 import { emailRouter } from './email.router';
 
 export const routes = express.Router();
